0bfe6e59f7e03a9a25d41082999987fc6a93a4ee
spavelv666 <spavelv666.gmail.com>
2020-10-29 18:04:58 +0300

    first commit

---
+++ movies/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Movie
+
+
+class MoviesView(View):
+   # '''Список фильмов'''
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movie_list.html", {'movie_list': movies})
